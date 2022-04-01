21cc1f747dce86d6bc1de07f68a109fd2b0a7cb5
Jeremy Lewi <jeremylewimth@gmail.com>
2022-04-01 20:39:35 +0700

    Revert "commit4"
    This reverts commit cfc95d01e3c9473834e6aa63409984d1f1d02fd7.

--- test.js
+++ test.js
@@ -2,7 +2,10 @@
 console.log("Hello World, this is a test");
 console.log("Tes commit ke-2");
 console.log("Tes push ke github");
+<<<<<<< HEAD
 console.log("Tes pull dari github");
 =======
 console.log('Hello World');
 >>>>>>> parent of a6f3173 (commit2)
+=======
+>>>>>>> parent of cfc95d0 (commit4)
